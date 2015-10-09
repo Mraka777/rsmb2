86318a093974b5082162e5a15af8dd22b6342d5a
DmitryL <dlyachin@gmail.com>
2015-10-09 15:34:56 +0300

    added data for local use

--- core/base.php
+++ core/base.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
 $dbname = "host1408042_bm"; // the name of the database that you are going to use for this project
-$dbuser = "host1408042_bm"; // the username that you created, or were given, to access your database
-$dbpass = "XMM_2nVT"; // the password that you created, or were given, to access your database
+$dbuser = "root"; // the username that you created, or were given, to access your database
+$dbpass = ""; // the password that you created, or were given, to access your database
 mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 ?>
