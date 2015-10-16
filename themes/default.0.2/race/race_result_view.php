0b64e49af79ae5ab81d633882ce6e3394624a742
DmitryL <dlyachin@gmail.com>
2015-10-16 20:06:50 +0300

    added data to template

--- themes/default.0.2/race/race_result_view.php
+++ themes/default.0.2/race/race_result_view.php
@@ -23,37 +23,43 @@
 										<tbody>
 											<tr>
 												<td style="font-weight:bold;">Date</td>
-												<td>1/1 (30 sep 2015 12:00)</td>
-												<td style="font-weight:bold;">Track Type</td> <td>Plain</td>
+												<td><?php echo($track_data['race_num_day']); ?>/35 (<?php echo($track_data['race_real_date']); ?>)</td>
+												<td style="font-weight:bold;">Track Type</td> <td><?php echo($track_data['track_type']); ?></td>
 											</tr>
 											<tr>
 												<td style="font-weight:bold;">Competition</td>
 												<td>
-													<a href=""><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
-													<a href="">League I.1</a> (Round 1)
+													<a href=""><img class="flag" src="/assets/images/flag/<?php echo($track_data['logo']); ?>" title="<?php echo($track_data['nameb_en']); ?>"></a>
+													<a href="/<?php print($language_link);?>/competition/league">League <?php echo($track_data['league_lvl']); ?>.<?php echo($track_data['league_num']); ?></a> (Round 1)
 												</td>
-												<td style="font-weight:bold;">Pl/Ri/Des</td> <td>60/20/20</td>
+												<td style="font-weight:bold;">Pl/Ri/Des</td> <td><?php echo($track_data['track_plain']); ?>/<?php echo($track_data['track_rise']); ?>/<?php echo($track_data['track_descent']); ?></td>
 											</tr>
 											<tr>
 												<td style="font-weight:bold;">Track</td>
 												<td>
-													<a href=""><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
-													<a href="">Test Team 1000</a>
+													<a href=""><img class="flag" src="/assets/images/flag/<?php echo($track_data['logo']); ?>" title="<?php echo($track_data['nameb_en']); ?>"></a>
+													<a href=""><?php echo($track_data['track_name']); ?></a>
 												</td>													
-												<td style="font-weight:bold;">Temperature</td> <td>-4<sup>o</sup>, Sunny</td>
+												<td style="font-weight:bold;">Temperature</td> <td><?php print($race_weather['temperature']); ?><sup>o</sup>, <?php print($race_weather['sun_type_descr']); ?></td>
 											</tr>
 											<tr>
 												<td style="font-weight:bold;">Home Club</td>
 												<td>
-													<a href=""><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
-													<a href="">Test track 3</a>
+													<a href="#"><img class="flag" src="/assets/images/flag/<?php echo($track_data['logo']); ?>" title="<?php echo($track_data['nameb_en']); ?>"></a>
+													<a href="/<?php print($language_link);?>/team/view/<?php echo($track_data['track_team_id']); ?>"><?php echo($track_data['track_team_name']); ?></a>
 												</td>
-												<td style="font-weight:bold;">Humidity&Wind</td> <td>90%, 5 m/s</td>
+												<td style="font-weight:bold;">Humidity&Wind</td> <td><?php print($race_weather['humidity']); ?>%, <?php print($race_weather['wind_speed']); ?> m/s</td>
 											</tr>
 											<tr>
-												<td style="font-weight:bold;">Attendance</td> <td>10000/10000</td>
-												<td style="font-weight:bold;">Precipitation</td> <td>Snow, No Fog</td>
+												<td style="font-weight:bold;">Attendance</td> <td><?php echo($track_data['race_attendance']); ?>/<?php echo($track_data['track_capacity']); ?></td>
+												<td style="font-weight:bold;">Precipitation</td> <td><?php print($race_weather['phenomena_descr']);?></td>
 											</tr>
+										
+											<!-- DELETE!!! 
+											<tr>
+												<td colspan = 4><?php //print_r($race_weather); ?></td>
+											</tr>
+											DELETE!!! -->
 										</tbody>
 									</table>
 								</div>
@@ -80,7 +86,7 @@
 												for($i=0;$i<8;$i++) { ?>
 												<tr>
 													<td style="font-weight:bold;"><?php echo ($i+1);?></td>
-													<td><img src="/images/flag/<?php echo $top8_team_pts[$i]['logo'];?>"> <a href="/en/team/view/<?php echo $top8_team_pts[$i]['team_id'];?>"><?php echo $top8_team_pts[$i]['team_name'];?></a></td>
+													<td><img src="/images/flag/<?php echo $top8_team_pts[$i]['logo'];?>"> <a href="/<?php print($language_link);?>/team/view/<?php echo $top8_team_pts[$i]['team_id'];?>"><?php echo $top8_team_pts[$i]['team_name'];?></a></td>
 													<td><?php echo $top8_team_pts[$i]['race_points'];?></td>
 												</tr>
 												<?php } ?>
@@ -108,7 +114,7 @@
 												for($i=0;$i<8;$i++) { ?>
 												<tr>
 													<td style="font-weight:bold;"><?php echo ($i+9);?></td>
-													<td><img src="/images/flag/<?php echo $next8_team_pts[$i]['logo'];?>"> <a href="/en/team/view/<?php echo $next8_team_pts[$i]['team_id'];?>"><?php echo $next8_team_pts[$i]['team_name'];?></a></td>
+													<td><img src="/images/flag/<?php echo $next8_team_pts[$i]['logo'];?>"> <a href="/<?php print($language_link);?>/team/view/<?php echo $next8_team_pts[$i]['team_id'];?>"><?php echo $next8_team_pts[$i]['team_name'];?></a></td>
 													<td><?php echo $next8_team_pts[$i]['race_points'];?></td>
 												</tr>
 												<?php } ?>
@@ -132,7 +138,7 @@
 													</td>
 														<td>
 														<a href="" title="Russia"><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
-														<a href="/en/player/view/12" title="Yaroslav Lebedev">Lebedev, Yaroslav</a>
+														<a href="/<?php print($language_link);?>/player/view/12" title="Yaroslav Lebedev">Lebedev, Yaroslav</a>
 													</td>
 												</tr>
 												<tr>
@@ -155,7 +161,7 @@
 													</td>
 														<td>
 														<a href="" title="Russia"><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="<?php print($race_sniper['nameb_en']); ?>"></a>
-														<a href="/en/player/view/<?php print($race_sniper['sportsman_id']); ?>" title="<?php print($race_sniper['name1']." ");print($race_sniper['name2']); ?>"><?php print($race_sniper['name1']." ");print($race_sniper['name2']); ?></a>
+														<a href="/<?php print($language_link);?>/player/view/<?php print($race_sniper['sportsman_id']); ?>" title="<?php print($race_sniper['name1']." ");print($race_sniper['name2']); ?>"><?php print($race_sniper['name1']." ");print($race_sniper['name2']); ?></a>
 													</td>
 												</tr>
 												<tr>
@@ -168,7 +174,7 @@
 									
 									<!-- Best Ski -->
 									<div class="panel panel-default">
-										<div class="panel-heading" style="padding:5px 10px 5px 10px;">Best Ski*</div>
+										<div class="panel-heading" style="padding:5px 10px 5px 10px;">Best Ski</div>
 										<table class="table-striped table-bordered table-condensed" style="width:100%;">
 											<tbody>
 												<tr></tr>
@@ -177,12 +183,12 @@
 														<span class="glyphicon glyphicon-user" style="font-size:3em;float:left;"></span>
 													</td>
 														<td>
-														<a href="" title="Russia"><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
-														<a href="/en/player/view/12" title="Yaroslav Lebedev">Lebedev, Yaroslav</a>
+														<a href="" title="<?php print($race_best_ski['nameb_en']); ?>"><img class="flag" src="/assets/images/flag/<?php print($race_best_ski['logo']); ?>" title="<?php print($race_best_ski['nameb_en']); ?>"></a>
+														<a href="/<?php print($language_link);?>/player/view/<?php print($race_best_ski['sportsman_id']); ?>" title="<?php print($race_best_ski['name1']." ");print($race_best_ski['name2']); ?>"><?php print($race_best_ski['name1']." ");print($race_best_ski['name2']); ?></a>
 													</td>
 												</tr>
 												<tr>
-													<td>Pure Ski result: 1:10:59.15</td>
+													<td>Pure Ski result: <?php echo gmdate('H:i:s', $race_best_ski['ski_time']); ?></td>
 												</tr>
 											</tbody>
 										</table>
@@ -227,25 +233,25 @@
 												<td><strong><?php echo($i+1);?></strong></td>
 												<td style="white-space: nowrap;">
 													<img src="/images/flag/icons-flag-ru.png">
-													<a href=""><?php echo $sportsman['name2']; ?></a>
+													<a href="/<?php echo($language_link); ?>/player/view/<?php echo $sportsman['sportsman_id']; ?>"><?php echo $sportsman['name2']; ?> <?php echo $sportsman['name1'][0]; ?>.</a>
 												</td>
 												<td style="white-space: nowrap;">
 													<img src="/images/flag/icons-flag-ru.png">
-													<a href=""><?php echo $sportsman['team_name']; ?></a>
+													<a href="/<?php echo($language_link); ?>/team/view/<?php echo $sportsman['team_id']; ?>"><?php echo $sportsman['team_name']; ?></a>
 												</td>
-												<td>1:14:11.15</td>
-												<td>+1:11.10</td>
+												<td><?php echo gmdate('H:i:s', $sportsman['overall_time']); ?></td>
+												<td><?php echo gmdate('H:i:s', $sportsman['behind']);?></td>
 												<td><strong><?php echo $sportsman['race_points']; ?></strong></td>
 												<th></th>
-												<td>1:10:10.15</td>
-												<td>1+0+1+0</td>
-												<td>2</td>
+												<td><?php echo gmdate('H:i:s', $sportsman['ski_time']); ?></td>
+												<td><?php echo( $sportsman['race_sh1']);?> + <?php echo( $sportsman['race_sh2']);?> + <?php echo( $sportsman['race_sh3']);?> + <?php echo( $sportsman['race_sh4']);?></td>
+												<td><?php echo($sportsman['race_sh1']+$sportsman['race_sh2']+$sportsman['race_sh3']+$sportsman['race_sh4']);?></td>
 												<th></th>
-												<td>+2</td>
-												<td>+2</td>
-												<td>+0</td>
-												<td>+1</td>
-												<td>9</td>
+												<td>-</td>
+												<td>-</td>
+												<td>-</td>
+												<td>-</td>
+												<td>-</td>
 											</tr>
 										<?php
 										$i++;
