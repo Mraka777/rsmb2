0d9b7309adec927b2688e1b9ce20988bcf7c60b5
DmitryL <dlyachin@gmail.com>
2015-10-16 20:06:20 +0300

    Added energy influence

--- core/simple.php
+++ core/simple.php
@@ -186,11 +186,15 @@ while ($races_total = mysql_fetch_assoc($result)) {
   }
   
   
-  $sql = "SELECT `track_type`, `track_tech` FROM `rsm_track` WHERE `track_id` = $track_id ";
+  $sql = "SELECT `track_type`, `track_tech`, `track_capacity` FROM `rsm_track` WHERE `track_id` = $track_id ";
   //echo($sql."<br>");
   $res=mysql_query($sql) or die(mysql_error());
   While ($track = mysql_fetch_row($res))
   {
+    //attendance
+    $race_attendance = rand(50,$track[2]);
+    //print("ATT=".$attendance);
+    
     switch ($track[0]) {
       case 1:
           $track_type=1;
@@ -226,12 +230,14 @@ while ($races_total = mysql_fetch_assoc($result)) {
   LEFT OUTER JOIN rsm_sportsman ON rsm_race_sportsman_list.sportsman_id = rsm_sportsman.sportsman_id
   LEFT OUTER JOIN rsm_team ON rsm_sportsman.team_id = rsm_team.team_id
   LEFT OUTER JOIN rsm_country ON rsm_sportsman.country_id = rsm_country.country_id
-  WHERE race_id='.$race_id.' ';//LIMIT 0, 3
+  LEFT OUTER JOIN rsm_race_sportsman_tactics ON (rsm_sportsman.sportsman_id = rsm_race_sportsman_tactics.sportsman_id AND rsm_race_sportsman_list.race_id = rsm_race_sportsman_tactics.race_id)
+  WHERE rsm_race_sportsman_list.race_id = '.$race_id.' ';//LIMIT 0, 3
   
   $res=mysql_query($sql) or die(mysql_error());
   //echo($sql);
     While ($b_data = mysql_fetch_array($res))
       {//наполняем массив
+        //print_r($b_data);
         $penalty_time[$i]=array();
         //$b_data['phys_energy']=$b_data['phys_energy']/10;
         $b_data['phys_strength']=$b_data['phys_strength'];
@@ -241,6 +247,28 @@ while ($races_total = mysql_fetch_assoc($result)) {
         $b_data['shoot_calm']=$b_data['shoot_calm']/10;
         $b_data['track_tech']=$b_data['track_tech'];
         $b_data['track_spd']=$b_data['track_spd'];
+        //$b_data['phys_energy']=$b_data['track_spd'];
+        //print("SID=".$b_data['sportsman_id']." NRG=".$b_data['phys_energy']." IMP=".$b_data['rsm_race_sportsman_tactics_importance']."<br>");
+        //IMPORTANCE
+        $d_imp = $b_data['rsm_race_sportsman_tactics_importance']*5;
+        
+        $k_nrg = $b_data['phys_energy']/100;
+        $new_nrg = $b_data['phys_energy'] - $d_imp;
+        
+        switch ($b_data['rsm_race_sportsman_tactics_importance']) {
+          case 1:
+            $k_imp = 1;
+            break;
+          case 2:
+            $k_imp = 1.05;
+            break;
+          case 3:
+            $k_imp = 1.1;
+            break;
+        }
+        
+        $sql = "UPDATE  `rsm_sportsman` SET  `phys_energy` =  '".$new_nrg."' WHERE  `rsm_sportsman`.`sportsman_id` =".$b_data['sportsman_id'].";";
+        mysql_query($sql);
         //$penalty=0;
         //$b_data[$b_data['sportsman_id']]['shooting']=array();
       //добавляем состояние
@@ -250,6 +278,7 @@ while ($races_total = mysql_fetch_assoc($result)) {
         //echo('************************************************************<br>');
         //echo('<b>Shooting</b><br>');
         $i=1; $j=1;
+        $sh_data[1]=0;$sh_data[2]=0;$sh_data[3]=0;$sh_data[4]=0;
         for($i=1; $i<=$global['shooting_total']; $i++) {
         //echo('<b>Lap '.$i.'</b><br>');
           for($j=1; $j<=$global['shooting_fires']; $j++) {
@@ -258,15 +287,20 @@ while ($races_total = mysql_fetch_assoc($result)) {
             $shooting_acc=$b_data['shoot_tech']*$global['shooting_technique']+$b_data['shoot_acc']*$global['shooting_accuracy'];
             //echo("SH_ACC=".$b_data['shoot_tech']."<br>");
             if ($var<$shooting_acc) {  //записываем в массив лога стрельбы попал или нет - $bir[$bid]['shooting_log'][$lap][$shoot_num]
+              
               $sql_shot_insert = 'INSERT INTO `rsm_logs` (`logs_id`, `race_id`, `sportsman_id`, `lap`, `shot`, `res`) VALUES (NULL, '.$race_id.', '.$b_data['sportsman_id'].', '.$i.', '.$j.', 1)';
               mysql_query($sql_shot_insert);
               //echo('1 '); //Попадание
             } 
             else {
+              //промах
               $sql_shot_insert = 'INSERT INTO `rsm_logs` (`logs_id`, `race_id`, `sportsman_id`, `lap`, `shot`, `res`) VALUES (NULL, '.$race_id.', '.$b_data['sportsman_id'].', '.$i.', '.$j.', 0)';
               mysql_query($sql_shot_insert);
               $penalty[$i]=$penalty[$i]+1;
               $misses=$misses+1;
+              
+              $sh_data[$i] = $sh_data[$i] + 1;
+              
               if (($i=='1') or ($i=='3')) {
                 $misses_lay=$misses_lay+1;
                 //echo('LAY MISS<br>');
@@ -275,6 +309,7 @@ while ($races_total = mysql_fetch_assoc($result)) {
               //echo($penalty[$i]);
               //echo('0 '); //Промах
             }
+            //данные для sportsman_list стрельба на i рубеже
             
           }
           //echo('Total miss 1='.$misses_lay);
@@ -290,7 +325,7 @@ while ($races_total = mysql_fetch_assoc($result)) {
      //echo('MISSES='.$misses);echo('<br>');
      //echo('MISSES_LAY='.$misses_lay);echo('<br>');
      //echo('MISSES_STAND='.$misses_stand);echo('<br>');
-     $sql = 'UPDATE `rsm_race_sportsman_list` SET `race_shots_missed` = '.$misses.', `race_shots_stand` = '.$misses_stand.', `race_shots_lay` = '.$misses_lay.' WHERE `sportsman_id` = '.$b_data['sportsman_id'].' AND `race_id` = '.$race_id.' ';
+     $sql = 'UPDATE `rsm_race_sportsman_list` SET `race_sh1` = '.$sh_data[1].', `race_sh2` = '.$sh_data[2].', `race_sh3` = '.$sh_data[3].', `race_sh4` = '.$sh_data[4].',`race_shots_missed` = '.$misses.', `race_shots_stand` = '.$misses_stand.', `race_shots_lay` = '.$misses_lay.' WHERE `sportsman_id` = '.$b_data['sportsman_id'].' AND `race_id` = '.$race_id.' ';
      mysql_query($sql);
      //echo($sql.'<br>');
       //echo('************************************************************<br>'); 
@@ -377,6 +412,9 @@ while ($races_total = mysql_fetch_assoc($result)) {
       //echo("Speed_result= ".$speed_result."<br>");
       //echo("weather_total= ".$weather_total."<br>");
       $speed_result=$weather_total*$speed_result;
+      //NRG EFFECT
+      $speed_result = $speed_result * $k_nrg * $k_imp;
+      
       //echo("Speed_result + weather = ".$speed_result."<br>");
       //$speed_result = 1;
       
@@ -483,7 +521,6 @@ while ($races_total = mysql_fetch_assoc($result)) {
       //echo($sql.'<br>');
       $i++;
     }
-  
     
   $sql = "SELECT * FROM `rsm_race_sportsman_list` WHERE `race_id` = $race_id ORDER BY `race_points` DESC"  ;
   //echo($sql.'<br>');
@@ -554,7 +591,7 @@ while ($races_total = mysql_fetch_assoc($result)) {
   
   //print_r($total_points);
   
-  $sql = "UPDATE `rsm_race` SET `race_status` = '1' WHERE `race_id` = $race_id";
+  $sql = "UPDATE `rsm_race` SET `race_status` = '1', `race_attendance` = '".$race_attendance."' WHERE `race_id` = $race_id";
   //echo($sql.'<br>');
   mysql_query($sql);
   
@@ -681,6 +718,33 @@ while ($races_total = mysql_fetch_assoc($result)) {
     $i++;
     }
   }
+  $sql = "SELECT DISTINCT(sportsman_id) FROM rsm_logr WHERE `race_id` = $race_id";
+  $res=mysql_query($sql) or die(mysql_error());
+  While ($temp_sp_id = mysql_fetch_assoc($res)){
+    //print($temp_sp_id['sportsman_id']." ");
+    $temp_mark_array = array(3000, 2999, 6000, 5999, 9000, 8999, 12000, 11999, 15000);
+    
+    for ($i=0;$i<=8;$i++){
+      $sql2 = "SELECT time FROM `rsm_logr` WHERE  `race_id` = ".$race_id."  AND  `sportsman_id` = ".$temp_sp_id['sportsman_id']." AND mark = ".$temp_mark_array[$i]." ";
+      $res2=mysql_query($sql2) or die(mysql_error());
+      While ($temp_time = mysql_fetch_assoc($res2)){
+        //print("SP=".$temp_sp_id['sportsman_id']." M=".$temp_mark_array[$i]." ".$temp_time['time']."<br>");
+        $temp_ov_time[$i]=$temp_time['time'];
+      }
+
+    }
+      //print("<pre>");
+      //print_r($temp_ov_time);
+      //print("</pre>");
+     //print($sql."<br>");
+     //print_r($temp_ov_time);
+     $ski_time = $temp_ov_time[8]-($temp_ov_time[0]-$temp_ov_time[1])-($temp_ov_time[2]-$temp_ov_time[3])-($temp_ov_time[4]-$temp_ov_time[5])-($temp_ov_time[6]-$temp_ov_time[7]);
+     //print("SP=".$temp_sp_id['sportsman_id']." SKI TIME=".$ski_time."<br>");
+    $sql = "UPDATE  `rsm_race_sportsman_list` SET  `ski_time` =  '".$ski_time."', `overall_time` =  '".$temp_ov_time[8]."' WHERE  `race_id` = ".$race_id." AND `sportsman_id` = ".$temp_sp_id['sportsman_id'].";";
+    mysql_query($sql);
+    //print($sql."<br>");
+  }
+  
   $num_race_in_day++;
 }
 
