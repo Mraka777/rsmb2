21d03678bacfb1da091113d46d87476f3062dab1
Mraka <Mraka777@gmail.com>
2015-10-19 20:49:55 +0300

    New design 2.0

--- themes/default.0.2/infrastructure/infrastructure_index_view.php
+++ themes/default.0.2/infrastructure/infrastructure_index_view.php
@@ -6,22 +6,196 @@
 							<h1>Infrastructure overview</h1>
 							<div class="help-on-page"><?php include $rsm['th_path']."/_common/tpl-help-on-page.php";?></div>
 						</div>
-						<div class="panel-body">					
-							<table class="table-striped table-bordered table-condensed ">
-									<tr style="font-weight:bold;text-align:center;">
-											<td colspan=12>Infrastructure MENU</td>
-									</tr>	
-									<tr style="font-weight:bold;text-align:center;">
-											<td><a href="/<?php echo($rsm_base_url);?>/infrastructure/facilities/">Overview Facilities</a></td>
-											<td><a href="/<?php echo($rsm_base_url);?>/infrastructure/stadium/">Overview Stadium</a></td>
-									</tr>
-									<tr style="font-weight:bold;text-align:center;">
-											<td><a href="/<?php echo($rsm_base_url);?>/infrastructure/facilities/"><!-- <img src="/images/track/infrastructure_facilities_view.jpg"> --></a></td>
-											<td><a href="/<?php echo($rsm_base_url);?>/infrastructure/stadium/"><!-- <img src="/images/track/infrastructure_stadium_view.jpg"> --></a></td>
-									</tr>
-							
-								
-							</table> 
+						<div class="panel-body">
+							<div class="row clearfix">
+								<div class="col-md-6 column">
+									<img src="/assets/images/track/sample_track.gif" style="width:100%;margin-bottom:1em;">
+									<div class="panel panel-default">
+										<div class="panel-heading">Stadium 
+										<a href="/en/infrastructure/stadium" class="btn btn-success btn-xs" role="button">Rebuild <span class="glyphicon glyphicon-chevron-right"></span></a>
+										</div>
+										<table class="table-striped table-bordered table-condensed table-td1strong" style="width:100%">
+											<tbody>
+												<tr>
+													<td>Title</td>
+													<td>
+														<a href=""><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
+														<a href="">Test Track</a>
+													</td>
+													</td>
+												</tr>
+												<tr>
+													<td>Owner</td>
+													<td>
+														<a href=""><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
+														<a hreff="">Mraka</a>
+													</td>
+												</tr>
+												<tr>
+													<td>Home club</td>
+													<td>
+														<a href=""><img class="flag" src="/assets/images/flag/icons-flag-ru.png" title="Russia"></a>
+														<a href="">Moscow Steel Dragons</td>
+													</td>
+												</tr>
+												<tr>
+													<td>Capacity</td>
+													<td>10000</td> 
+												</tr>
+												<tr>
+													<td>General type</td>
+													<td>Plain</td>
+												</tr>
+												<tr>
+													<td>Pl / Ri / Des</td>
+													<td>60% / 20% / 20%</td>
+												</tr>
+												<tr>
+													<td>Difficulty</td>
+													<td>10</td>
+												</tr>
+												<tr>
+													<td colspan="2" class="rsm-table-row-naming" style="text-align:center;">Quality&PR</td>
+												</tr>
+												<tr>
+													<td>Media center</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Video panel</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Light</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Snow machines</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Shooting Range</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td colspan="2" class="rsm-table-row-naming" style="text-align:center;">Comfort&Commercial</td>
+												</tr>
+												<tr>
+													<td>Club Museum</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Shops</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Food court</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Parking</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+												<tr>
+													<td>Toilets</td>
+													<td><?php rate_stars($th_url,5,1);?></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<div class="col-md-6 column">
+									<img src="/assets/images/track/sample_track.gif" style="width:100%;margin-bottom:1em;">
+									<div class="panel panel-default">
+										<div class="panel-heading">Facilities 
+										<a href="/en/infrastructure/facilities" class="btn btn-success btn-xs" role="button">Rebuild <span class="glyphicon glyphicon-chevron-right"></span></a>
+										</div>
+										<table class="table-striped table-bordered table-condensed table-td1strong" style="width:100%">
+											<thead>
+												<tr>
+													<th>Facility</th>
+													<th>Rate</th>
+													<th>Lvl</th>
+													<th>E1</th>
+													<th>E2</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td colspan="5" class="rsm-table-row-naming" style="text-align:center;">Sport departments</td>
+												</tr>
+												<tr>
+													<td style="width:40%"><span class="glyphicon glyphicon-education"></span> Training base</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-home"></span> Service Dept.</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-home"></span> Youth Academy</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												
+												<tr>
+													<td colspan="5" class="rsm-table-row-naming" style="text-align:center;">Club departments</td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-home"></span> Club Office</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-search"></span> Scouting dept.</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-plus-sign"></span> Medical Complex</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												
+												<tr>
+													<td colspan="5" class="rsm-table-row-naming" style="text-align:center;">Economic departments</td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-usd"></span> Economy dept.</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td><span class="glyphicon glyphicon-wrench"></span> Maintaince dept.</td>
+													<td style="width:80px;"><?php rate_stars($th_url,5,1);?></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+
+											</tbody>
+										</table>
+									</div>
+								</div>
+								</div>
+							</div>
+						
 						</div>
 					</div>
 					<!-- /Content -->				
